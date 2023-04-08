9d867be339012da3f5a73e16f70ccfc74c3a3447
kirill <da.deffoxz@gmail.com>
2023-04-08 10:55:52 +0300

    module 12 task 1

--- js/util.js
+++ js/util.js
@@ -12,3 +12,33 @@ export const request = async (url, options) => {
 
   return response.json();
 };
+
+/**
+ *
+ * @param {(...args: any) => any} callback
+ * @param {number} [delay]
+ * @returns {(...args: any) => any}
+ */
+export const debounce = (callback, delay = 500) => {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+  let lastCallTime;
+
+  return (...rest) => {
+    const elapsedTime = Date.now() - lastCallTime;
+    const newDelay = Math.max(delay - elapsedTime, 0);
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => {
+      callback(...rest);
+      lastCallTime = Date.now();
+    }, newDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+};
