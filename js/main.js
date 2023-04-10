56b99998e798f5d29471cda844a6cdd8a960bf65
kirill <da.deffoxz@gmail.com>
2023-04-10 10:35:06 +0300

    исправления от наставника

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ import openStatusPopup from './status-popup.js';
 import { request } from './util.js';
 import './upload.js';
 
+const url = 'https://28.javascript.pages.academy/kekstagram/data';
+
 try {
   /**
    * @type {PictureState[]} data
    */
-  const data = await request('https://28.javascript.pages.academy/kekstagram/data');
+  const data = await request(url);
 
   initGallery(data);
 
